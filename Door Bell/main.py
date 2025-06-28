99aaeafaf1ac863e45d88fa43f4239ae22813c0c
Yeasin Arafat <122679771+arafat69691@users.noreply.github.com>
2025-06-28 22:11:55 +0600

    Update main.py
    change playsound and use pygame package

--- Door Bell/main.py
+++ Door Bell/main.py
@@ -1,22 +1,37 @@
 
+
+
 import cv2
 import face_recognition
-import playsound
 import threading
+import pygame
+
+
+pygame.mixer.init()
+pygame.mixer.music.load("doorbell-223669.mp3")  
+
 cap = cv2.VideoCapture(0)
-sound_plaued = False
+sound_played = False
 
 def ring_bell():
-    playsound.playsound('doorbell-223669.mp3')
-while True:
-    _ , frame = cap.read()
-    faces = face_recognition.face_locations(frame)
-    if faces and not sound_plaued :
-        threading.Thread(target=ring_bell).start()
-        sound_plaued = True
-    elif not faces and sound_plaued:
-        sound_plaued = False
-    cv2.imshow('Ai Doorbell',frame)
+    pygame.mixer.music.play()
 
-    if cv2.waitKey(10) ==ord('q'):
+while True:
+    _, frame = cap.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    faces = face_recognition.face_locations(rgb_frame)
+
+    if faces and not sound_played:
+        threading.Thread(target=ring_bell).start()
+        sound_played = True
+    elif not faces and sound_played:
+        sound_played = False
+
+    cv2.imshow('AI Doorbell', frame)
+
+    if cv2.waitKey(10) == ord('q'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
+
